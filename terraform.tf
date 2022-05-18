66e9f148a1144d7042da07e2354fa7051bf31f8f
Toby <tobyclemson@gmail.com>
2022-05-18 15:51:22 +0100

    Allow version 4 terraform AWS provider.

--- terraform.tf
+++ terraform.tf
@@ -4,7 +4,8 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = "~> 3.39"
+      version = ">= 3.39"
+
       configuration_aliases = [
         aws.certificate,
         aws.domain_validation,
